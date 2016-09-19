009dd36b7f7465b40cd55ce09552c5dc3f295a76
Alexandre <alexandre.leibler@gmail.com>
2016-09-19 16:28:46 -0400

    create static function to find the id, and made respective test pass

--- src/Task.php
+++ src/Task.php
@@ -2,10 +2,12 @@
 
     class Task{
         private $description;
+        private $id;
 
-        function __construct($description)
+        function __construct($description, $id=null)
         {
             $this->description = $description;
+            $this->id = $id;
         }
 
         function getDescription()
@@ -18,10 +20,17 @@
             $this->description = (string) $new_description;
 
         }
+        function getID()
+        {
+            return $this->id;
+        }
+        //NOTE: no setter, probably because in memory we want to create tasks with no ID and SQL will assign the IDs in the database... maybe
 
         function save()
         {
             $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+            //NOTE: this will sync the local id with the SQL ID
+            $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
         static function getAll()
@@ -44,7 +53,9 @@
             {
                 $description = $database_data[$task_index]['description'];
                 //   var_dump($description);
-                $new_task = new Task($description);
+                //NOTE: this is getting the id from the database
+                $id = $database_data[$task_index]['id'];
+                $new_task = new Task($description, $id);
                 //NOTE: This is the same as array push:
                 $tasks[] = $new_task;
             }
@@ -55,6 +66,20 @@
         static function deleteAll()
         {
             $GLOBALS['DB']->exec("DELETE FROM tasks;");
+        }
+
+        static function find($search_id)
+        {
+            $found_task = null;
+            $tasks = Task::getAll();
+            for ($task_index = 0; $task_index < count($tasks); $task_index++){
+                $current_id = $tasks[$task_index]->getID();
+                if ($current_id === $search_id){
+                    return $tasks[$task_index];
+                }
+            }
+            print("Could not find task with id: " . $search_id . "\n");
+            return null;
         }
 
     }
--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -13,6 +13,26 @@
 
     class TaskTest extends PHPUnit_Framework_TestCase
     {
+
+        //NOTE: CLEAN UP DATABASE!!!
+        protected function teardown()
+        {
+            Task::deleteAll();
+        }
+
+        function test_getID()
+        {
+            //ARRANGE
+            $description = 'wash the cat';
+            $id = 1;
+            $expected_output = $id;
+            $test_task = new Task($description, $id);
+            //ACT
+            $result = $test_task->getID();
+            //ASSERT
+            $this->assertEquals($expected_output, $result);
+        }
+
         function test_save()
         {
             //ARRANGE
@@ -26,7 +46,7 @@
             $result = Task::getAll();
             //$result[0] takes the first instance inside of the results array (references tasks in task.php)
             $this->assertEquals($expected_output, $result[0]);
-            Task::deleteAll();
+            $this->teardown();
 
             // print("test_task: ");
             // var_dump($test_task);
@@ -66,19 +86,47 @@
 
             //ASSERT
             $this->assertEquals($expected_output, $result);
-            Task::deleteAll();
+            $this->tearDown();
         }
 
-        // function test_deleteAll()
-        // {
-        //     //ARRANGE
-        //     $description = 'wash the cat';
-        //     $description2 = 'wash the dog';
-        //     $test_task = new Task ($description);
-        //     $test_task->save();
-        //     $test_task2 = new Task ($description);
-        //     $test_task->save();
-        //
-        // }
+        function test_deleteAll()
+        {
+            //ARRANGE
+            $description = 'wash the cat';
+            $description2 = 'wash the dog';
+            $test_task = new Task ($description);
+            $test_task->save();
+            $test_task2 = new Task ($description);
+            $test_task2->save();
+            $expected_output = [];
+
+            //ACT
+            Task::deleteAll();
+            $result = Task::getAll();
+
+            //ASSERT
+            $this->assertEquals($expected_output, $result);
+            $this->tearDown();
+        }
+
+        function test_find()
+        {
+            //ARRANGE
+            $description = 'wash the cat';
+            $description2 = 'wash the dog';
+            $test_task = new Task ($description);
+            $test_task->save();
+            $test_task2 = new Task ($description2);
+            $test_task2->save();
+            $expected_output = $test_task;
+
+            //ACT
+            $id = $test_task->getID();
+            $result = Task::find($id);
+
+            //ASSERT
+            $this->assertEquals($expected_output, $result);
+
+        }
     }
 ?>
