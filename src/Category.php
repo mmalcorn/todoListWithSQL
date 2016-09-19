99fb7597cf6bf489425a24e8be9da4773a8c2e33
Alexandre <alexandre.leibler@gmail.com>
2016-09-19 16:59:03 -0400

    finish the src for category table

--- src/Category.php
+++ src/Category.php
@@ -31,6 +31,42 @@
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
+        static function getAll()
+        {
+            $database_categories = $GLOBALS['DB']->query("SELECT * FROM categories;");
+            $database_data = $database_categories->fetchAll();
+            $categories =  array();
+
+            for ($category_index = 0; $category_index < count($database_data); $category_index++)
+            {
+                $name = $database_data[$category_index]['name'];
+                $id = $database_data[$category_index]['id'];
+                $new_category = new Category($name, $id);
+                $categories[] = $new_category;
+            }
+
+            return $categories;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM categories;");
+        }
+
+        static function find($search_id)
+        {
+            $found_category = null;
+            $categories = Category::getAll();
+            for ($category_index = 0; $category_index < count($categories); $category_index++){
+                $current_id = $categories[$category_index]->getID();
+                if ($current_id === $search_id){
+                    return $categories[$category_index];
+                }
+            }
+            print("Could not find task with id: " . $search_id . "\n");
+            return null;
+        }
+
     }
 
 
